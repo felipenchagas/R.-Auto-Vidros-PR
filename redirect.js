790bbe96ac97d18e73d681e38fab6e1182406920
felipenchagas <felipe@empresarialweb.com.br>
2024-07-23 23:43:12 -0300

    w

--- redirect.js
+++ redirect.js
@@ -1,5 +1,12 @@
 (function() {
-  if (window.location.pathname.endsWith('/')) {
-    window.location.href = window.location.pathname.slice(0, -1) + window.location.search + window.location.hash;
+  try {
+    if (window.location.pathname.endsWith('/')) {
+      var newPath = window.location.pathname.slice(0, -1) + window.location.search + window.location.hash;
+      if (newPath !== window.location.href) {
+        window.location.replace(newPath);
+      }
+    }
+  } catch (error) {
+    console.error("Redirection script error:", error);
   }
 })();
