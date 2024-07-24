552b6e60e4710d121d7e3816551ada3aaee5f481
felipenchagas <felipe@empresarialweb.com.br>
2024-07-24 00:05:08 -0300

    d

--- redirect.js
+++ redirect.js
@@ -1,19 +1,19 @@
 (function() {
   try {
+    console.log("Current URL:", window.location.href);
     // Verifica se a URL termina com uma barra
     if (window.location.pathname.endsWith('/')) {
       // Remove a barra final
-      var newPath = window.location.protocol + '//' + window.location.host + window.location.pathname.slice(0, -1) + window.location.search + window.location.hash;
+      var newPath = window.location.origin + window.location.pathname.slice(0, -1) + window.location.search + window.location.hash;
+      console.log("New Path:", newPath);
 
       // Redireciona apenas se a nova URL for diferente da atual
       if (newPath !== window.location.href) {
-        console.log("Redirecionando para:", newPath); // Adiciona log para depuração
+        console.log("Redirecionando para:", newPath);
         window.location.replace(newPath);
-      } else {
-        console.log("URL já está correta:", window.location.href); // Log para quando não há redirecionamento
       }
     } else {
-      console.log("URL não termina com barra:", window.location.href); // Log para URLs sem barra final
+      console.log("URL não termina com barra:", window.location.href);
     }
   } catch (error) {
     console.error("Redirection script error:", error);
